5083b668acd697f96952bf80b7daaa6dbb28b497
JLevanoArbizu <jose.levano@vallegrande.edu.pe>
2019-09-10 14:06:09 -0500

    :apple: DEMOOOO :construction:

---
+++ src/main/java/controller/TareaC.java
@@ -0,0 +1,108 @@
+package controller;
+
+import dao.ImplTAREA;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import model.TAREA;
+
+@Named(value = "tareaC")
+@SessionScoped
+public class TareaC implements Serializable {
+
+    TAREA tarea = new TAREA();
+    private TAREA selectedTarea;
+    private List<TAREA> lstTarea;
+
+    @PostConstruct
+    public void iniciar() {
+        try {
+            listarTarea();
+        } catch (Exception e) {
+        }
+    }
+
+    public void limpiarTarea() throws Exception {
+        tarea = new TAREA();
+    }
+
+    public void registrarTarea() {
+        ImplTAREA dao;
+        try {
+            dao = new ImplTAREA();
+            dao.registrar(tarea);
+            listarTarea();
+            limpiarTarea();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registrado Correctamente", null));
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar" + e, null));
+            System.out.println("errrrorr" + e);
+        }
+    }
+
+    public void modificarTarea() throws Exception {
+        ImplTAREA dao;
+        try {
+            dao = new ImplTAREA();
+            dao.modificar(selectedTarea);
+            listarTarea();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Modificado Correctamente", null));
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Modificar" + e, null));
+        }
+
+    }
+
+    public void eliminarTarea() throws Exception {
+        ImplTAREA dao;
+        try {
+            dao = new ImplTAREA();
+            dao.eliminar(selectedTarea);
+            listarTarea();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Eliminado Correctamente", null));
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Eliminar" + e, null));
+        }
+    }
+
+    public void listarTarea() throws Exception {
+        ImplTAREA dao;
+        try {
+            dao = new ImplTAREA();
+            lstTarea = dao.listar();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public TAREA getTarea() {
+        return tarea;
+    }
+
+    public void setTarea(TAREA tarea) {
+        this.tarea = tarea;
+    }
+
+    public TAREA getSelectedTarea() {
+        return selectedTarea;
+    }
+
+    public void setSelectedTarea(TAREA selectedTarea) {
+        this.selectedTarea = selectedTarea;
+    }
+
+    public List<TAREA> getLstTarea() {
+        return lstTarea;
+    }
+
+    public void setLstTarea(List<TAREA> lstTarea) {
+        this.lstTarea = lstTarea;
+    }
+    
+    
+
+}
